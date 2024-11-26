01e01417c3269aeb0bdd21060ca5a6a8d3d08a1f
JosueVJ <josuevj.1e.49@gmail.com>
2024-11-26 00:27:05 -0600

    se alejan cositas

--- backend/controllers/nuevo.js
+++
@@ -1,8 +0,0 @@
-const prueba = async (req, res) => {
-	sesion = req.session;
-	if (sesion.usuario) {
-		res.status(200).json({ message: "Usuario autenticado." });
-	} else {
-		res.status(401).json({ message: "Usuario no autenticado." });
-	}
-};
