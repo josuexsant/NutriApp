86129a734e0a68576f501045af7aebe1056d693f
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-11-27 17:24:59 -0600

    rebase hecho

--- backend/controllers/nutriologoController.js
+++ backend/controllers/nutriologoController.js
@@ -1,4 +1,4 @@
-import connectDB from '../models/DataBase.js'; // Importa la conexión
+import connectDB from "../models/DataBase.js"; // Importa la conexión
 
 const verificarUnicidadNutriologo = async (req, res) => {
   const { correo, cedula, telefono } = req.body;
@@ -8,27 +8,35 @@ const verificarUnicidadNutriologo = async (req, res) => {
 
     // Ejecutar el procedimiento almacenado
     const [rows] = await connection.execute(
-      'CALL verificar_unicidad_nutriologo(?, ?, ?)', // Utiliza los placeholders (?) para los parámetros
+      "CALL verificar_unicidad_nutriologo(?, ?, ?)", // Utiliza los placeholders (?) para los parámetros
       [correo, cedula, telefono] // Los valores que pasas al procedimiento
     );
 
     // Si el procedimiento no lanza errores, el registro es único
-    res.status(200).json({ message: 'El registro es único y válido.' });
+    res.status(200).json({ message: "El registro es único y válido." });
     connection.end(); // Cierra la conexión después de la consulta
   } catch (error) {
     // Manejar errores personalizados del procedimiento almacenado
-    if (error.code === 'ER_DUP_ENTRY') {
-      res.status(400).json({ error: 'El nutriólogo ya está registrado.' });
+    if (error.code === "ER_DUP_ENTRY") {
+      res.status(400).json({ error: "El nutriólogo ya está registrado." });
     } else {
-      res.status(500).json({ error: 'Error interno del servidor.' });
+      res.status(500).json({ error: "Error interno del servidor." });
     }
   }
 };
 
 const guardarNutriologo = async (req, res) => {
   const {
-    nombre, apellido_pat, apellido_mat, telefono, ciudad_residencia,
-    codigo_postal, cedula_profesional, correo_electronico, contrasena, token
+    nombre,
+    apellido_pat,
+    apellido_mat,
+    telefono,
+    ciudad_residencia,
+    codigo_postal,
+    cedula_profesional,
+    correo_electronico,
+    contrasena,
+    token,
   } = req.body;
 
   try {
@@ -36,16 +44,26 @@ const guardarNutriologo = async (req, res) => {
 
     // Ejecutar el procedimiento almacenado
     const [rows] = await connection.execute(
-      'CALL guardar_nutriologo(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-      [nombre, apellido_pat, apellido_mat, telefono, ciudad_residencia,
-      codigo_postal, cedula_profesional, correo_electronico, contrasena, token]
+      "CALL guardar_nutriologo(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        nombre,
+        apellido_pat,
+        apellido_mat,
+        telefono,
+        ciudad_residencia,
+        codigo_postal,
+        cedula_profesional,
+        correo_electronico,
+        contrasena,
+        token,
+      ]
     );
 
-    res.status(200).json({ message: 'Nutriólogo registrado exitosamente.' });
+    res.status(200).json({ message: "Nutriólogo registrado exitosamente." });
     connection.end(); // Cierra la conexión después de la consulta
   } catch (error) {
     // Manejar errores del procedimiento almacenado
-    res.status(500).json({ error: 'Error interno del servidor.' });
+    res.status(500).json({ error: "Error interno del servidor." });
   }
 };
 
@@ -57,23 +75,35 @@ const autenticarNutriologo = async (req, res) => {
 
     // Ejecutar el procedimiento almacenado
     const [rows] = await connection.execute(
-      'CALL autenticar_nutriologo(?, ?, @token)',
+      "CALL autenticar_nutriologo(?, ?, @token)",
       [correo_electronico, contrasena]
     );
 
     // Obtener el token
-    const [tokenQuery] = await connection.execute('SELECT @token AS token');
+    const [tokenQuery] = await connection.execute("SELECT @token AS token");
 
     if (tokenQuery[0].token) {
       res.status(200).json({ token: tokenQuery[0].token });
     } else {
-      res.status(400).json({ error: 'Correo electrónico o contraseña incorrectos.' });
+      res
+        .status(400)
+        .json({ error: "Correo electrónico o contraseña incorrectos." });
     }
 
     connection.end(); // Cierra la conexión después de la consulta
   } catch (error) {
     // Manejar errores del procedimiento almacenado
-    res.status(500).json({ error: 'Error interno del servidor.' });
+    res.status(500).json({ error: "Error interno del servidor." });
+  }
+};
+
+export const login = (req, res) => {
+  if (req.body.email === "josue@gmail.com" && req.body.password === "1234") {
+    console.log("Usuario logeado");
+    res.status(200).json({ message: "Usuario logeado" });
+  } else {
+    console.log("Usuario no logeado");
+    res.status(400).json({ error: "Usuario no logeado" });
   }
 };
 
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,17 @@
 import { Router } from "express";
 export const router = Router();
+import { login } from "../controllers/nutriologoController.js";
 
 // Escribe aqui tus rutas...
 router.post("/register", () => {
   console.log("endpoint register");
+});
+
+router.post("/login", (req, res) => {
+  const status = login(req, res);
+  if (status === 200) {
+    res.status(200).json({ message: "Usuario logeado" });
+  } else {
+    res.status(400).json({ message: "Usuario no logeado" });
+  }
 });
