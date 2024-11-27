2053823587c1c73247a4060f0baf45057ff096a7
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-11-27 12:14:58 -0600

    tablas regimen_consumoDiario modificadas

---
+++ backend/config/__tests__/regimenController.test.js
@@ -0,0 +1,63 @@
+import connectDB from '../../models/DataBase.js'; // Importa la conexión
+
+// Controlador para agregar un régimen de consumo diario
+const agregarRegimenConsumoDiario = async (req, res) => {
+  const {
+    pacienteId, diasJson, caloriasPorGrupoJson, alimentosJson
+  } = req.body;
+
+  try {
+    const connection = await connectDB(); // Establece la conexión
+
+    // Ejecutar el procedimiento almacenado para el régimen de consumo diario
+    const [rows] = await connection.execute(
+      'CALL agregar_regimen_consumo_diario(?, ?, ?, ?)',
+      [
+        pacienteId, 
+        JSON.stringify(diasJson),  // Se asegura de que los JSON se envíen como cadenas JSON
+        JSON.stringify(caloriasPorGrupoJson), 
+        JSON.stringify(alimentosJson)
+      ]
+    );
+
+    res.status(200).json({ message: 'Régimen de consumo diario registrado exitosamente.' });
+    connection.end(); // Cierra la conexión después de la consulta
+  } catch (error) {
+    // Manejar errores del procedimiento almacenado
+    res.status(500).json({ error: 'Error interno del servidor.' });
+  }
+};
+
+// Controlador para agregar un régimen por tiempos
+const agregarRegimenTiempos = async (req, res) => {
+  const {
+    pacienteId, limiteCalorias, tiemposComidaJson, caloriasPorGrupoJson, alimentosTiemposJson
+  } = req.body;
+
+  try {
+    const connection = await connectDB(); // Establece la conexión
+
+    // Ejecutar el procedimiento almacenado para el régimen por tiempos
+    const [rows] = await connection.execute(
+      'CALL agregar_regimen_tiempos(?, ?, ?, ?, ?)',
+      [
+        pacienteId, 
+        limiteCalorias, 
+        JSON.stringify(tiemposComidaJson),  // Se asegura de que los JSON se envíen como cadenas JSON
+        JSON.stringify(caloriasPorGrupoJson), 
+        JSON.stringify(alimentosTiemposJson)
+      ]
+    );
+
+    res.status(200).json({ message: 'Régimen por tiempos registrado exitosamente.' });
+    connection.end(); // Cierra la conexión después de la consulta
+  } catch (error) {
+    // Manejar errores del procedimiento almacenado
+    res.status(500).json({ error: 'Error interno del servidor.' });
+  }
+};
+
+export default {
+  agregarRegimenConsumoDiario,
+  agregarRegimenTiempos
+};
